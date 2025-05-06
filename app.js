881b31f613b3e9f9146ce37f01889b9ac6dc27cd
angiemiku <angiemiku0@gmail.com>
2025-05-06 12:07:17 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+let btn = document.querySelector("button");
+let para = document.querySelector("p");
+
+btn.addEventListener("click", async function () {
+  try {
+    left = await getData();
+    para.innerText = left;
+  } catch (err) {
+    para.innerText = "An error has occur :(";
+  }
+});
+
+let link = 'https://catfact.ninja/fact';
+
+async function getData() {
+  let res = await axios.get(link);
+  console.log(res.data.fact);
+  return res.data.fact;
+}
